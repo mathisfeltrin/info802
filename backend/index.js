c58aaf2b2d9efa0332b375ac7a6fd358f2209f2d
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2025-02-10 17:20:26 +0100

    fix

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,9 +9,6 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// Serve static files from the frontend directory
-app.use(express.static(path.join(__dirname, "frontend")));
-
 // Routes
 app.use("/api/stations", require("./routes/stations"));
 app.use("/api/map", require("./routes/map"));
@@ -20,10 +16,9 @@ app.use("/api/vehicles", require("./routes/vehicles"));
 
 // Route par défaut
 app.get("/", (req, res) => {
-  // res.send(
-  //   "Bienvenue sur l'API Agrégatrice pour les véhicules électriques ! 🚗⚡"
-  // );
-  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+  res.send(
+    "Bienvenue sur l'API Agrégatrice pour les véhicules électriques ! 🚗⚡"
+  );
 });
 
 // Lancer le serveur
